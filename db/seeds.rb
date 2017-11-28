40400a63955792853c30a63a713b886d0d04f7c6
Forrest Cranmer <letosleepingdragon@gmail.com>
2017-11-28 15:57:01 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  teacher = User.create(username: Faker::LeagueOfLegends.unique.champion, email: Faker::Internet.email, phone_number: Faker::Number.number(10))
+  student = User.create(username: Faker::HarryPotter.unique.character, email: Faker::Internet.email, phone_number: Faker::Number.number(10))
+  a = Assignment.create(content: Faker::VentureBros.quote, voice: Faker::VentureBros.organization)
+  b = Submission.create(content: Faker::VForVendetta.quote, voice: Faker::StarWars.quote )
+  c = Grading.create(content: Faker::WorldOfWarcraft.quote, voice: Faker::HarryPotter.quote)
+  teacher.teaching_lessons.create(assignment: a, submission: b, grading: c, student: student)
+end
