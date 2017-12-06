ca703b724fa3625bdeb375f97a045807eb1362c8
Forrest Cranmer <letosleepingdragon@gmail.com>
2017-12-06 20:41:34 +0800

    avatar url added to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205135349) do
+ActiveRecord::Schema.define(version: 20171206124019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20171205135349) do
     t.string "username"
     t.string "open_id"
     t.boolean "is_teacher"
+    t.string "avatar"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
