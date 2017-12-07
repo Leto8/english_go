38d3465c959269db60af63a92b78a756a9ec17eb
Forrest Cranmer <letosleepingdragon@gmail.com>
2017-12-07 18:35:17 +0800

    gr

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # # This file should contain all the record creation needed to seed the database with its default values.
 
-User.destroy_all
 Lesson.destroy_all
+User.destroy_all
 Assignment.destroy_all
 Submission.destroy_all
 Grading.destroy_all
